462ff2da51577ddc4fa2d5bbee2632c12f748253
fejes.patrik <fejes.patrik@BBG336>
2024-09-11 15:38:12 +0200

    Add project files.

---
+++ Proba/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("{0}. Fejes Patrik ", i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
