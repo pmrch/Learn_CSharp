53a571a6f2353576d0e2ad222da655912ecef629
pmrch <fejespatrik2007@gmail.com>
2024-09-16 15:58:04 +0200

    Created new project

---
+++ Valami/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Valami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Üdv neked!");
+            Console.Write("Hány éves vagy? ");
+
+            string yr = Console.ReadLine();
+            int num_of_yrs = int.Parse(yr);
+
+            if (num_of_yrs < 14)
+            {
+                Console.Write("Jé, hogyhogy középsikolás vagy?");
+            }
+            else
+            {
+                Console.Write("Egy év múlva {0} éves leszel.", num_of_yrs + 1);
+            }
+            //Console.ReadKey();
+
+        }
+    }
+}
