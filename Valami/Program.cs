b06809cb2a094e2404c304b7c1e54586658fc28d
pmrch <fejespatrik2007@gmail.com>
2024-09-16 16:50:20 +0200

    Óra vége

--- Valami/Program.cs
+++ Valami/Program.cs
@@ -6,6 +6,7 @@ namespace Valami
     {
         static void Main(string[] args)
         {
+            // Eredeti feladat
             Console.WriteLine("Üdv neked!");
             Console.Write("Hány éves vagy? ");
 
@@ -20,8 +21,44 @@ namespace Valami
             {
                 Console.Write("Egy év múlva {0} éves leszel.", num_of_yrs + 1);
             }
+            Console.WriteLine(" ");
             //Console.ReadKey();
 
+            // Matyi feladat
+            Console.Write("Mi volt Mátyás király keresztneve? ");
+            string valasz = Console.ReadLine();
+
+            if (valasz == "Mátyás")
+            {
+                Console.WriteLine("Valóban.");
+            }
+            else
+            {
+                Console.WriteLine("Hát majdnem");
+            }
+
+            // Csillag
+            string csillag = new string('*', 10);
+            Console.WriteLine(csillag);
+            Console.WriteLine(" ");
+
+            // Összetett típus és tömb (NEM LISTA)
+
+            int[] tomb = {3, 5, 2, 1};
+            // Tömbnél előre kell tudni a méretét
+            // Ha számokkal meg akarunk tölteni egy tömböt, azt az előzőben láthatjuk
+
+            // tomb = [2, 4, 6, 1];
+            string szoveg = "vízalatti pacsirta csapda";
+
+            Console.WriteLine(szoveg[5]);
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                Console.WriteLine(tomb[i]);
+            }
+            Console.WriteLine(tomb[2]);
+
+            Console.ReadKey();
         }
     }
 }
