fefc2f991c2a23d73363c7a4939690ae21f690d8
pmrch <fejespatrik2007@gmail.com>
2024-11-18 16:57:59 +0100

    Latest

--- Választás/Program.cs
+++ Választás/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Transactions;
 
 namespace Valasztas
 {
@@ -27,9 +29,101 @@ namespace Valasztas
             }
             foreach (var item in kepviselok)
             {
-                Console.Write("{0}\t{1}\t{2}\t{3}\t{4}", 
+                Console.Write("{0}\t{1}\t{2}\t{3}\t{4}\n", 
                 item.korzet, item.szavazat, item.vnev, item.knev, item.part);
-                Console.WriteLine();
+            }
+            Console.WriteLine("\n2. Feladat\nA helyhatósági választáson {0} képviselőjelölt indult.\n", kepviselok.Count());
+            Console.WriteLine("3. Feladat");
+
+            string elonev = Console.ReadLine();
+            string utonev = Console.ReadLine();
+            int szav = 0;
+            bool talalat = false;
+
+            foreach (var item in kepviselok)
+            {
+                if (item.vnev == elonev && item.knev == utonev)
+                {
+                    szav = item.szavazat;
+                    talalat = true;
+                }
+            }
+            
+            if (szav > 0 && talalat == true)
+            {
+                Console.WriteLine($"{elonev} {utonev} {szav} szavazatot kapott.");
+            }
+            else
+            {
+                Console.WriteLine("Ilyen nevű képviselőjelölt nem szerepel a nyilvántartásban.");
+            }
+            Console.WriteLine("\n4. Feladat");
+
+            float osszeg = 0;
+            float szazalek;
+            const int ossz_polgar = 12345;
+
+            foreach (var item in kepviselok)
+            {
+                osszeg += item.szavazat;
+            }
+            szazalek = (osszeg / ossz_polgar) * 100;
+            Console.WriteLine($"A választáson {osszeg} állampolgár, a jogosultak {Math.Round(szazalek, 2)}%-a vett részt.\n\n5. Feladat");
+            
+            // 5. Feladat
+            int gyep = 0;
+            int hep = 0;
+            int tisz = 0;
+            int zep = 0;
+            int fugg = 0;
+
+            foreach (var item in kepviselok)
+            {
+                if (item.part == "GYEP")
+                {
+                    gyep += item.szavazat;
+                }
+                else if (item.part == "HEP")
+                {
+                    hep += item.szavazat;
+                }
+                else if (item.part == "TISZ")
+                {
+                    tisz += item.szavazat;
+                }
+                else if (item.part == "ZEP")
+                {
+                    zep += item.szavazat;
+                }
+                else
+                {
+                    fugg += item.szavazat;
+                }
+            }
+            Console.WriteLine($"Gyümölcsevők pártja= {Math.Round((float)gyep / osszeg * 100, 2)}%");
+            Console.WriteLine($"Húsevők pártja= {Math.Round((float)hep / osszeg * 100, 2)}%");
+            Console.WriteLine($"Tejivók szövetsége= {Math.Round((float)tisz / osszeg * 100, 2)}%");
+            Console.WriteLine($"Zöldségevők pártja= {Math.Round((float)zep / osszeg * 100, 2)}%");
+            Console.WriteLine($"Független jelöltek= {Math.Round((float)fugg / osszeg * 100, 2)}%\n");
+
+            // 6. Feladat
+            Console.WriteLine("6. Feladat");
+            List<int> szavazatok_szama = new List<int>();
+
+            foreach (var item in kepviselok)
+            {
+                szavazatok_szama.Add(item.szavazat);
+            }
+
+            int legtobb_szavazat = szavazatok_szama.Max();
+            Console.WriteLine(legtobb_szavazat);
+
+            for (int i = 0; i < kepviselok.Count(); i++)
+            {
+                if (kepviselok[i].szavazat == legtobb_szavazat)
+                {
+                    Console.WriteLine($"{kepviselok[i].vnev} {kepviselok[i].knev} {kepviselok[i].part}");
+                }
             }
         }
     }
