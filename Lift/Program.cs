de2d82729e10bc7cace101c4a802f36f952dce89
pmrch <fejespatrik2007@gmail.com>
2024-12-19 22:37:32 +0100

    Lift

---
+++ Lift/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Lift;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 1. Feladat
+        List<Lift> igenyek = new List<Lift>();
+        int[] alapadatok = new int[3];
+        int index = 0;
+
+        foreach(var item in File.ReadAllLines("igeny.txt"))
+        {
+            if (item.Contains(" "))
+            {
+                igenyek.Add(new Lift(item));
+            }
+            else
+            {
+                alapadatok[index] = int.Parse(item);
+            }
+            index++;
+        }
+
+        // 2. Feladat
+        Console.Write("\n2. Adja meg a kezdő szint számát: ");
+        int induloszint = int.Parse(Console.ReadLine());
+
+        // 3. Feladat
+        var vegso_szint = igenyek.Last().end;
+        Console.WriteLine("\n3. A lift a {0}. szinten áll az utolsó igény teljesítése után.", vegso_szint);
+
+        // 4. Feladat
+        int max = igenyek.Max(p => Math.Max(p.start, p.end));
+        int min = igenyek.Min(p => Math.Min(p.start, p.end));
+
+        Console.WriteLine("\n4. Feladat\nA legmagasabb szint: {0}\nA legalacsonyabb szint: {1}", max, min);
+
+        // 5. Feladat
+        var count = igenyek.Where(p => p.start < p.end).Count();
+        var count2 = igenyek.Where(p => p.end < p.start).Count();
+        Console.WriteLine($"\nUtassal: {count}  Utas nélkül: {count2}");
+
+        // 6. Feladat
+        HashSet<int> csapatok = new HashSet<int>();
+        foreach(var item in igenyek) csapatok.Add(item.sorszam);
+        
+        string nem_vette_igenybe = "";
+        foreach(var item in nem_vette_igenybe)
+
+        Console.WriteLine("\nNem vette igénybe a liftet: {0}", nem_vette_igenybe);
+    }
+}
---
+++ Lift/Program2.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Lift_lista
+{
+    class Program
+    {
+        private struct igény
+        {
+            public int óra, perc, másodperc, csapat, honnan, hová;
+        }
+        private static igény[] igények = new igény[100];
+        private static int szintszám, csapatszám, igényszám;
+        private static int indulószint;
+        private static Random rnd = new Random();
+        private static int véletlencsapat=3;
+
+        static void nMain(string[] args)
+        {
+            Feladat1();
+            Feladat2();
+            Feladat3();
+            Feladat4();
+            Feladat5();
+            Feladat6();
+            Feladat7();
+            Feladat8();
+            Console.ReadLine();
+        }
+
+        static void Feladat1()
+        {
+            FileStream fájl = new FileStream("igeny.txt", FileMode.Open);
+            StreamReader olvasó = new StreamReader(fájl);
+            string[] egysor = new string[6];
+
+            Console.WriteLine("1. Az adatok beolvasása");
+            Console.WriteLine();
+            szintszám = int.Parse(olvasó.ReadLine());
+            csapatszám = int.Parse(olvasó.ReadLine());
+            igényszám = int.Parse(olvasó.ReadLine());
+
+            // Az i blokkváltozó lesz
+            for (int i = 0; i < igényszám; i++)
+            {
+                egysor = olvasó.ReadLine().Split(' ');
+                igények[i].óra = int.Parse(egysor[0]);
+                igények[i].perc = int.Parse(egysor[1]);
+                igények[i].másodperc = int.Parse(egysor[2]);
+                igények[i].csapat = int.Parse(egysor[3]);
+                igények[i].honnan = int.Parse(egysor[4]);
+                igények[i].hová = int.Parse(egysor[5]);
+              }
+            fájl.Close();
+        }
+        static void Feladat2()
+        {
+            Console.Write("2. Adja meg, hogy kezdetben melyik szinten van a lift=");
+            indulószint = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+        }
+
+        static void Feladat3()
+        {
+        Console.WriteLine("3. A lift a " + igények[igényszám-1].hová + " szinten áll az utolsó igény teljesítése után.");
+        Console.WriteLine();
+        }
+
+        static void Feladat4()
+        {
+            int alsó = indulószint;
+            int felső = indulószint;
+            int i;
+
+            for (i = 0; i < igényszám; i++)
+            {
+                alsó = Math.Min(alsó, Math.Min(igények[i].honnan, igények[i].hová));
+                felső = Math.Max(felső, Math.Max(igények[i].honnan, igények[i].hová));
+            }
+            //Kiíráskor sztringgé alakítjuk
+            Console.WriteLine("4. Alsó szint = " + alsó.ToString() + " felső szint = " + felső.ToString());
+            Console.WriteLine();
+        }
+
+        static void Feladat5()
+        {
+            int utassal = 0;
+            int üresen = 0;
+            int előzőszint = indulószint;
+            
+            // i blokkváltozó lesz
+            for (int i = 0; i < igényszám; i++)
+            {
+                if (igények[i].honnan < igények[i].hová)
+                {
+                    //A változó inkrementálása
+                    utassal++;
+                }
+                if (előzőszint < igények[i].honnan) üresen++;
+                előzőszint = igények[i].hová;
+            }
+            //Megegyezik a VB-vel
+            Console.WriteLine("5. Utassal= {0} üresen= {1}", utassal, üresen);
+            Console.WriteLine();
+        }
+
+        static void Feladat6()
+        {
+            int[] darab = new int[csapatszám + 1];
+
+            for (int i =0; i<igényszám; i++)
+            {
+                darab[igények[i].csapat]++;
+            }
+
+            Console.Write("6. Nem vette igénybe a liftet: ");
+            for (int i = 1; i <= csapatszám; i++)
+                if (darab[i] == 0)
+                {
+                    Console.Write(i.ToString() + " ");
+                }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+       
+      static void Feladat7()
+      {
+      int holvanéppen  = -1;         //Kezdetben nem tudjuk hol van
+      bool csalte = false;
+
+      véletlencsapat = rnd.Next(1, csapatszám);
+      Console.WriteLine("7. Kiválasztott csapat=" + véletlencsapat);
+
+      for (int i = 0; i<igényszám; i++)
+      {
+          if (igények[i].csapat == véletlencsapat)
+          {
+              if (holvanéppen == -1)
+              {
+                  holvanéppen = igények[i].hová;
+              }
+              else
+              {
+                  if (holvanéppen != igények[i].honnan)
+                  {
+                      Console.WriteLine("Csalás a(z) " + i + ". igénynél, lement " + holvanéppen + "->" + igények[i].honnan);
+                      csalte = true;
+                  }
+              }
+              holvanéppen = igények[i].hová;
+          }
+       }
+          if (!csalte) Console.WriteLine("Nem állapítható meg csalás");
+          Console.WriteLine();
+      }
+
+       static void Feladat8()
+        { 
+           FileStream fájl = new FileStream("../../blokkol.txt", FileMode.Create);
+           StreamWriter író = new StreamWriter(fájl);
+
+           Console.WriteLine("8. Az adatok kiírása");
+           Console.WriteLine();
+ 
+           for (int i=0; i< igényszám; i++)
+            {
+                if (igények[i].csapat == véletlencsapat)
+                {
+                    Console.WriteLine("Befejezés ideje: " + igények[i].óra + ":" + igények[i].perc + ":" + igények[i].másodperc);
+                    író.WriteLine("Befejezés ideje: " + igények[i].óra + ":" + igények[i].perc + ":" + igények[i].másodperc);
+                    Console.Write("Sikeresség=");
+                    string sikeresség = Console.ReadLine();
+                    író.WriteLine("Sikeresség: " + sikeresség);
+                    Console.WriteLine("-----");
+                    író.WriteLine("-----");
+                    Console.WriteLine("Indulási emelelet: " + igények[i].honnan);
+                    író.WriteLine("Indulási emelelet: " + igények[i].honnan);
+                    Console.WriteLine("Célemelet: " + igények[i].hová);
+                    író.WriteLine("Célemelet: " + igények[i].hová);
+                    Console.Write("Feladatkód=");
+                    int feladatkód = int.Parse(Console.ReadLine());
+                    író.WriteLine("Feladatkód: " + feladatkód);
+                }
+            }
+        író.Close();
+        fájl.Close();
+
+        Console.WriteLine();
+        }
+    }
+}
+
+
+      
+    
